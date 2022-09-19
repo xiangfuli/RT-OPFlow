0aebe6967ce6c691eda5b2eff718619d14980d00
xiangfuli <xiangfuli96@gmail.com>
2022-09-18 22:47:01 -0400

    Add usb module support

---
+++ include/system/usb/usbd_conf.h
@@ -0,0 +1,12 @@
+#ifndef USBD_CONF_H
+#define USBD_CONF_H
+
+#include "usb_conf.h"
+
+#define USBD_CFG_MAX_NUM                1
+#define USBD_ITF_MAX_NUM                1
+
+#define USB_MAX_STR_DESC_SIZ            255
+
+
+#endif /* USBD_CONF_H */
